b3603c16e6c912068ce40541626dede5c3bf946b
fullm <matoma1_@naver.com>
2024-04-29 10:57:14 +0900

    회원 등록

--- src/main/java/com/example/filter/controller/UserApiController.java
+++ src/main/java/com/example/filter/controller/UserApiController.java
@@ -1,15 +1,19 @@
 package com.example.filter.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.filter.model.UserRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user")
+@RequiredArgsConstructor
+@Slf4j
 public class UserApiController {
 
-    @GetMapping("")
-    public void register(){
-
+    @PostMapping("")
+    public void register(@RequestBody UserRequest userRequest){
+        log.info("{}", userRequest);
     }
 }
--- src/main/java/com/example/filter/model/UserRequest.java
+++ src/main/java/com/example/filter/model/UserRequest.java
@@ -1,4 +1,21 @@
 package com.example.filter.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequest {
+
+    private String name;
+    private String phoneNumber;
+    private int age;
+
 }
