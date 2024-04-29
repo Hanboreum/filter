28f084d89c9c7fd56ef39301c2a79dba43bbf73a
fullm <matoma1_@naver.com>
2024-04-29 11:02:53 +0900

    클라이언트의 요청 보기 : echo

--- src/main/java/com/example/filter/controller/UserApiController.java
+++ src/main/java/com/example/filter/controller/UserApiController.java
@@ -3,7 +3,6 @@ package com.example.filter.controller;
 import com.example.filter.model.UserRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class UserApiController {
 
-    @PostMapping("")
+    /*@PostMapping("")
     public void register(@RequestBody UserRequest userRequest){
         log.info("{}", userRequest);
-    }
+    }*/
+
+   /* 디버깅 임시로 찍어보는 방법
+    @PostMapping("")
+    public void register( HttpEntity http){
+        log.info("{}",http.getBody() );
+    }*/
+
+   /* echo로 내리는 방법. 실질적으로 어떻게 정확하게 내려갔는지 알 방법이 없다
+   @PostMapping("")
+    public UserRequest register(@RequestBody UserRequest userRequest){
+        log.info("{}",userRequest );
+        return userRequest;
+    }*/
 }
